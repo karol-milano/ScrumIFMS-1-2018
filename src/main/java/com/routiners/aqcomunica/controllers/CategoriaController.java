a4af24eeb52b65a86222b077314873f106ef3a54
Jeferson Santos <jefersonsantos.jsm@gmail.com>
2018-05-27 23:24:20 -0400

    Cadastro de novas categorias pelo Usuário

---
+++ src/main/java/com/routiners/aqcomunica/controllers/CategoriaController.java
@@ -0,0 +1,28 @@
+package com.routiners.aqcomunica.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.routiners.aqcomunica.models.Categoria;
+import com.routiners.aqcomunica.repository.CategoriaRepository;
+
+@Controller
+public class CategoriaController {
+
+	
+	@Autowired
+	private CategoriaRepository cr;
+	
+	@RequestMapping(value="/cadastrarCategoria", method=RequestMethod.GET)
+	public String cadastrarUsuario() {
+		return "cadastrarCategoria";
+	}
+	
+	@RequestMapping(value="/cadastrarCategoria", method=RequestMethod.POST)
+	public String cadastrarUsuario(Categoria categoria) {
+		cr.save(categoria);
+		return "redirect:/";
+	}
+}
